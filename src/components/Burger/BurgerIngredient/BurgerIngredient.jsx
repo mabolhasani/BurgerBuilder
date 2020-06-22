19d25bee43b658b5be2f8126f4ed2468c56b95b8
mohammad <mohammad.abolhasany@gmail.com>
2020-06-22 16:59:28 +0430

    added prop types lib, added proptype to burger ingredient

--- src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./BurgerIngredient.module.css";
+import PropTypes from "prop-types";
 
 const BurgerIngredient = (props) => {
   let ingredient = null;
@@ -32,4 +33,9 @@ const BurgerIngredient = (props) => {
   }
   return ingredient;
 };
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
 export default BurgerIngredient;
