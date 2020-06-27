b3ec14483fddce304629f54dc3c3bea6d054ced7
mohammad <mohammad.abolhasany@gmail.com>
2020-06-27 21:21:44 +0430

    added total price to client ui

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -11,6 +11,9 @@ const controls = [
 const BuildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
+      <p>
+        Current price: <strong>{props.price.toFixed(2)}</strong>
+      </p>
       {controls.map((control) => {
         return (
           <BuildControl
