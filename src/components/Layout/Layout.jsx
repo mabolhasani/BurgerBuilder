cfe2e8589c4bb0ae70bee26bf251cd21aeade809
mohammad <mohammad.abolhasany@gmail.com>
2020-06-28 13:31:11 +0430

    added basic toolbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => {
   return (
     <React.Fragment>
-      <div>Toolbar, SideDrawe, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{props.children}</main>
     </React.Fragment>
   );
